93966c5ce29aecb2936fd496acd400605e286536
Fernando Seiji <fseiji@gmail.com>
2019-02-26 12:56:59 -0300

    Atualizacao utilizando somente JsonIgnore

--- src/main/java/com/fseiji/cursomc/domain/Endereco.java
+++ src/main/java/com/fseiji/cursomc/domain/Endereco.java
@@ -9,32 +9,32 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String logradouro;
 	private String numero;
 	private String complemento;
 	private String bairro;
 	private String cep;
-	
-	@JsonBackReference
+
+	@JsonIgnore
 	@ManyToOne
-	@JoinColumn(name="cliente_id")
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
-	@JoinColumn(name="cidade_id")
+	@JoinColumn(name = "cidade_id")
 	private Cidade cidade;
-	
-	public Endereco() {		
+
+	public Endereco() {
 	}
 
 	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
@@ -138,7 +138,5 @@ public class Endereco implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
