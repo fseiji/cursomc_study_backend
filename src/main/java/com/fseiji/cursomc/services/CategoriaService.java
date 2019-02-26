0a2dcb7e8f6a7ce7969b5455d1c2d03289966056
Fernando Seiji <fseiji@gmail.com>
2019-02-26 20:58:00 -0300

    Listando todas categorias com DTO

--- src/main/java/com/fseiji/cursomc/services/CategoriaService.java
+++ src/main/java/com/fseiji/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.fseiji.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,23 +23,27 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId());		
+		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-			repo.deleteById(id);	
+			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		}		
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
